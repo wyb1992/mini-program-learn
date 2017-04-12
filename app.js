83d225242f02df836c0f400694fa8030387096ed
wyb1992 <wyb496926024@gmail.com>
2017-04-12 16:01:48 +0800

    edit config

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 //app.js
 App({
-    globalData: {
-        baseUrl: "https://test02.1qizhuang.com/bingohome-dev/",
-        userInfo: {},
-        chooseCompanyId: 2516,
-        longitude: 121.473701,
-        latitude: 31.230416,
-        windowHeight: null,
-    }
+    globalData: {}
 })
